eea21dbff94eaa6d2566c00576e2b28604f711df
Maurilio Lucena <maurilioz1@hotmail.com>
2021-11-18 22:22:35 -0300

    final

--- scripts.js
+++ scripts.js
@@ -1,78 +1,66 @@
+const getBloodTypeName = (bloodId) => {
+    if (bloodId === 'a1') {
+        return 'A +';
+    } else if (bloodId === 'a0') {
+        return 'A -';
+    } else if (bloodId === 'b1') {
+        return 'B +';
+    } else if (bloodId === 'b0') {
+        return 'B -';
+    } else if (bloodId === 'ab1') {
+        return 'AB +';
+    } else if (bloodId === 'ab0') {
+        return 'AB -';
+    } else if (bloodId === 'o1') {
+        return 'O +';
+    } else if (bloodId === 'o0') {
+        return 'O -';
+    }
+}
+
 const bloodTypes = [
     {
         id: 'a1',
-        type: 'A+'
+        type: 'A +'
     },
     {
         id: 'a0',
-        type: 'A-'
+        type: 'A -'
     },
     {
         id: 'b1',
-        type: 'B+'
+        type: 'B +'
     },
     {
         id: 'b0',
-        type: 'B-'
+        type: 'B -'
     },
     {
         id: 'ab1',
-        type: 'AB+'
+        type: 'AB +'
     },
     {
         id: 'ab0',
-        type: 'AB-'
+        type: 'AB -'
     },
     {
         id: 'o1',
-        type: 'O+'
+        type: 'O +'
     },
     {
         id: 'o0',
-        type: 'O-'
+        type: 'O -'
     }
 ];
 
-const users = [
-    {
-        name: 'Maurilio Lucena',
-        email: 'maurilioz1@hotmail.com',
-        bloodType: 'A+'
-    },
-    {
-        name: 'Lucas Paulino',
-        email: 'lucas@hotmail.com',
-        bloodType: 'O-'
-    },
-    {
-        name: 'Thalys Melicio',
-        email: 'thalys@hotmail.com',
-        bloodType: 'AB+'
-    },
-]
+var users = [];
 
 var select = document.querySelector('select');
 var ul = document.querySelector('ul');
 
-users.map(user => {
-    var userName = document.createTextNode(user.name);
-    var userEmail = document.createTextNode(user.email).nodeValue;
-    var userBloodType = document.createTextNode(`( ${user.bloodType} )`);
-
-    var a = document.createElement('a');
-    var li = document.createElement('li');
-    var spanUserBloodType = document.createElement('span');
-    spanUserBloodType.appendChild(userBloodType);
-    var spanUserName = document.createElement('span');
-    spanUserName.appendChild(userName);
-
-    ul.appendChild(a);
-    a.appendChild(li);
-    li.appendChild(spanUserBloodType);
-    li.appendChild(spanUserName);
-
-    console.log(li);
-})
+var userName = document.querySelector('#name');
+var email = document.querySelector('#email');
+var bloodType = document.querySelector('#bloodType');
 
 bloodTypes.map(bloodType => {
     var bloodTypeId = document.createTextNode(bloodType.id).nodeValue;
@@ -84,3 +72,33 @@ bloodTypes.map(bloodType => {
 
     select.appendChild(option);
 });
+
+const handleSubmit = () => {
+    event.preventDefault();
+
+    const newUser = {
+        name: userName.value,
+        email: email.value,
+        bloodType: getBloodTypeName(bloodType.value),
+    }
+
+    users.push(newUser);
+
+    var a = document.createElement('a');
+    var li = document.createElement('li');
+    var spanUserBloodType = document.createElement('span');
+    var spanUserName = document.createElement('span');
+
+    var bloodTypeText = document.createTextNode(newUser.bloodType);
+    var userNameText = document.createTextNode(newUser.name);
+
+    a.setAttribute('href', `mailto:${newUser.email}`)
+
+    spanUserBloodType.appendChild(bloodTypeText);
+    spanUserName.appendChild(userNameText);
+
+    ul.appendChild(a);
+    a.appendChild(li);
+    li.appendChild(spanUserBloodType);
+    li.appendChild(spanUserName);
+}
